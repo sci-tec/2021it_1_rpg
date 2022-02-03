9aeee6ce9c5a78542a17f05994c0d8a36fd9743b
Tatsuki <2021it09@sci-tec.ac.jp>
2022-02-03 15:02:32 +0900

    マップ更新

--- js/field/maps/forest.js
+++ js/field/maps/forest.js
@@ -2,7 +2,7 @@ export default {
     images: [
         "cj,ac,ac,ac,ac,ac,ac,ac,ac,ac,ac,ac,ac,ac,ac,ac,ac,ac,cj",
         "cj,ac,ac,ac,ac,ac,ac,ac,ac,ac,ac,ac,ac,ac,ac,ac,ac,ac,cj",
-        "cj,ad,ad,ad,ad,ad,ah,ah,ah,ah,ah,ah,ah,ad,ad,ad,ad,ad,cj",
+        "cj,ac,ac,ac,ac,ac,ah,ah,ah,ah,ah,ah,ah,ac,ac,ac,ac,ac,cj",
         "cj,ah,ah,ah,ah,ah,ah,ah,ah,ah,ah,ah,ah,ah,ah,ah,ah,ah,cj",
         "cj,ah,ah,ah,ah,ah,ah,ah,ah,ae,ah,ah,ah,ah,ah,ah,ah,ah,cj",
         "cj,ah,ah,ah,ah,ah,ah,ah,ah,ah,ah,ah,ah,ah,ah,ah,ah,ah,cj",
@@ -18,7 +18,7 @@ export default {
      events: [
          {
              x: 0, y: 4,
-             goto: { name: "zyoukamati", x: 4, y: 6 }
+             goto: { name: "zyoukamati", x: 2, y: 11 }
          }
     //     {
     //         x: 14, y: 5,
