5ea151db1be11e9738271dad7b7c9b7ead8c0c9b
ichiyasa-g <chanhiro@karimatanoMacBook-Pro.local>
2022-02-03 14:20:24 +0900

    world and tika update

--- js/field/maps/world.js
+++ js/field/maps/world.js
@@ -1,29 +1,28 @@
 export default {
     images: [
         "ct,ct,ct,ct,ct,ct,ct,ct,ct,ct,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa",
-        "ct,ct,ct,ct,ct,ak,ak,ak,ak,ak,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa",
-        "ct,ct,ct,ct,ct,ak,ak,cu,ak,ak,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa",
-        "ct,ct,ct,ct,ct,ak,ak,ak,ak,ak,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa",
-        "ct,ct,ct,ct,ct,ak,ak,ak,ak,ak,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa",
-        "ct,ct,ct,ct,ct,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa",
-        "ct,ct,ct,ct,ct,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa",
-        "ct,ct,ct,ct,ct,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa",
-        "ct,ct,ct,ct,ct,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
-        "aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
-        "aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
-        "aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
-        "aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
-        "aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,aa,aa,aa,aa,aa,bf,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
-        "aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,aa,bj,bj,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
-        "aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
-        "aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,aa,bg,bh,bi,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
-        "aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
-        "aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
-        "aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,ah,ah,af",
-        "aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,ah,ah,af",
-        "aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,ah,ah,af",
-        "aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af"
-    
+"ct,ct,ct,ct,ct,ak,ak,ak,ak,ak,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa",
+"ct,ct,ct,ct,ct,ak,ak,cu,ak,ak,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa",
+"ct,ct,ct,ct,ct,ak,ak,ak,ak,ak,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa",
+"ct,ct,ct,ct,ct,ak,ak,ak,ak,ak,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa",
+"ct,ct,ct,ct,ct,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa",
+"ct,ct,ct,ct,ct,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa",
+"ct,ct,ct,ct,ct,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa",
+"ct,ct,ct,ct,ct,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
+"aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
+"aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
+"aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
+"aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
+"aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,aa,aa,aa,aa,be,bf,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
+"aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,aa,bj,bj,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
+"aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,be,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
+"aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,aa,bg,bh,bi,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
+"aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
+"aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
+"aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,ah,ah,af",
+"aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,ah,ah,af",
+"aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,ah,ah,af",
+"aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,aa,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af,af",
     ],
     sounds: {
         bgm: {
@@ -31,21 +30,21 @@ export default {
         }        
     },
     events: [
-        //{
-        //    x: 2, y: 2, 
-        //    goto: { name: "town2", x: 3, y: 1 }
-        //},
-        //{
-        //    x: 2, y: 8,
-        //    goto: { name: "town1", x: 3, y: 1 }
-        //},
-        //{
-        //    x: 2, y: 12,
-        //    enemy: { id: 1 }
-        //},
         {
-            x: 5, y: 5,
-            goto: { name: "zyoukamati", x: 8, y: 1 }
+            x: 26, y: 13, 
+            goto: { name: "mura", x: 25, y: 1 }
+        },
+        {
+            x: 22, y: 15,
+            goto: { name: "mura", x: 0, y: 8 }
+        },
+        {
+            x: 2, y: 12,
+            enemy: { id: 1 }
+        },
+        {
+            x: 7, y: 4,
+            goto: { name: "zyoukamati", x: 16, y: 13 }
         },
     ]
 }
